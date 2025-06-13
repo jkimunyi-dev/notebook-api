bde094e9ae81ed7a984693a2586851adb5d09838
Jimmy Kimunyi <jimmy@alluvium.net>
2025-06-13 17:17:42 +0300

    Note DTO's

---
+++ src/note/dto/create-note.dto.ts
@@ -0,0 +1,6 @@
+export class CreateNoteDTO {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: Date;
+}
---
+++ src/note/dto/update-note.dto.ts
@@ -0,0 +1,6 @@
+export class UpdateNoteDTO {
+  id?: string;
+  title?: string;
+  content?: string;
+  createdAt?: Date;
+}
