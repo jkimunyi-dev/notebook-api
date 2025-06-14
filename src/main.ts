df89e8204cdbfa67b8943b1929bda6013f329120
Jimmy Kimunyi <jimmy@alluvium.net>
2025-06-14 20:05:46 +0300

    Added class validation globally

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,5 +9,14 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      transform : true,
+      whitelist: true,
+      forbidNonWhitelisted: true
+    }
+  ))
 }
 bootstrap();
+ 
